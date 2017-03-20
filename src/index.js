6f766e56b3e9700eefe2ceba8f23bf11f7fc618d
Yan Li <y.yan.li@oracle.com>
2017-03-19 22:21:17 -0700

    init index.js

--- src/index.js
+++ src/index.js
@@ -13,18 +13,3 @@ ReactDOM.render(
     <App />
   </Provider>
   , document.querySelector('.container'));
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
