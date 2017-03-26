5a8dfde3b5c9e108d65a8f7a1109b2c031ab69f7
Yan Li <y.yan.li@oracle.com>
2017-03-26 10:39:09 -0700

    build form component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
+import { createPost } from '../actions/index';
+
 class PostsNew extends React.Component {
   render() {
     return (
       <div>
-          New Post Form
+        <h3>Create a New Post</h3>
+        <form>
+          <div className="form-group" >
+            <label for="title">Title</label>
+            <input className="form-control" type="text" />
+          </div>
+          <div className="form-group">
+            <label for="categories">Categories</label>
+            <input className="form-control" type="text" />
+          </div>
+          <div className="form-group">
+            <label for="description">Description</label>
+            <textarea className="form-control" />
+          </div>
+          <button className="btn btn-primary" type="submit">Submit</button>
+        </form>
       </div>
     );
   }
