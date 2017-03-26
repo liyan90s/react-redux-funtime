693ccbf96f50d5d5140d8a794a85f9c5a687fc5d
Yan Li <y.yan.li@oracle.com>
2017-03-26 10:38:31 -0700

    add redux-form reducer and post action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POST';
+const CREATE_POST = 'CREATE_POST';
 
 const ROOT_URL  = 'http://reduxblog.herokuapp.com/api';
 const API_KEY   = '?key=optimus21';
@@ -10,6 +11,15 @@ export function fetchPosts() {
 
   return {
     type: FETCH_POSTS,
+    payload: request
+  };
+}
+
+export function createPost(formData) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, formData);
+
+  return {
+    type: CREATE_POST,
     payload: request
   };
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import FetchPosts from './reducer_posts';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
-  posts: FetchPosts
+  posts: FetchPosts,
+  form: formReducer
 });
 
 export default rootReducer;
