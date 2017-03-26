bd0d05f8422416bbb18f92160e0ed05655d43a47
Yan Li <y.yan.li@oracle.com>
2017-03-26 16:12:33 -0700

    validation and redirect to index page after form submit

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,30 +1,89 @@
 import React from 'react';
+import { reduxForm } from 'redux-form';
+import { browserHistory, Link } from 'react-router';
 
 import { createPost } from '../actions/index';
 
 class PostsNew extends React.Component {
+  __backToIndex() {
+    browserHistory.push('/');
+  }
+
+  __handleOnSubmit(props) {
+    this.props.createPost(props)
+      .then(() => {
+        this.__backToIndex();
+      })
+  }
+
+  __hasError(field) {
+    return this.props.fields[field].touched && !!this.props.fields[field].error;
+  }
+
+  __renderError(field) {
+    return this.props.fields[field].touched ? this.props.fields[field].error : '';
+  }
+
   render() {
-    return (
+    return (      
       <div>
         <h3>Create a New Post</h3>
-        <form>
-          <div className="form-group" >
-            <label for="title">Title</label>
-            <input className="form-control" type="text" />
+        <form onSubmit={this.props.handleSubmit(this.__handleOnSubmit.bind(this))}>
+          <div className={"form-group " + (this.__hasError("title") ? "has-danger" : "")} >
+            <label htmlFor="title">Title</label>
+            <input className="form-control" type="text" {...this.props.fields.title}/>
+            <div>{this.__renderError("title")}</div>
           </div>
-          <div className="form-group">
-            <label for="categories">Categories</label>
-            <input className="form-control" type="text" />
+          <div className={"form-group " + (this.__hasError("categories") ? "has-danger" : "")} >
+            <label htmlFor="categories">Categories</label>
+            <input className="form-control" type="text" {...this.props.fields.categories} />
+            <div>{this.__renderError("categories")}</div>
           </div>
-          <div className="form-group">
-            <label for="description">Description</label>
-            <textarea className="form-control" />
+          <div className={"form-group " + (this.__hasError("content") ? "has-danger" : "")} >
+            <label htmlFor="content">content</label>
+            <textarea className="form-control" {...this.props.fields.content} />
+            <div>{this.__renderError("content")}</div>
           </div>
           <button className="btn btn-primary" type="submit">Submit</button>
+          <Link className="btn btn-secondary" type="button" onClick={this.__backToIndex}>Cancel</Link>
         </form>
       </div>
     );
   }
 }
 
-export default PostsNew;
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Enter a title';
+  }
+
+  if (!values.categories) {
+    errors.categories = 'Enter a category';
+  }
+
+  if (!values.content) {
+    errors.content = 'Enter a content';
+  }    
+
+  return errors;
+}
+
+export default reduxForm({
+  form: 'PostNewForm',   // this is the form name, shoule be a unique token
+  fields: ['title', 'categories', 'content'],
+  validate
+}, null, { createPost: createPost })(PostsNew);
+
+
+// user types sth ... in the inputs, which will be recored on application state
+// state === {
+//   form: {
+//     PostNewForm: {
+//       title: '...',
+//       categories: '...',
+//       content: '...'
+//     }
+//   }
+// }
